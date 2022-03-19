fba7416db10775c5dfa1ea8a36b057da7ae90b64
Aymene Lazreg <68120754+AymeneLazreg@users.noreply.github.com>
2022-03-19 01:51:52 +0100

    Update Homework-Basics-2-JS.js

--- Homework-Basics-2-JS.js
+++ Homework-Basics-2-JS.js
@@ -1,3 +1,15 @@
-arr = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
-arr.sort();
-console.log(arr);
+array = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+done = false;
+while (!done) 
+{
+    done = true;
+    for (i = 1; i < array.length; i += 1) {
+        if (array[i - 1] > array[i]) {
+            done = false;
+            tmp = array[i - 1];
+            array[i - 1] = array[i];
+            array[i] = tmp;
+        }
+    }
+}
+console.log(array);
